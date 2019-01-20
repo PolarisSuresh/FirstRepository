69fc761cd335d74402a9e3db31f32cb645741130
PolarisSuresh <polaris_suresh@yahoo.com>
2019-01-20 13:58:11 +0530

    Fourth Python

---
+++ fourthpython.py
@@ -0,0 +1,12 @@
+#!C:\Users\Suresh\AppData\Local\Programs\Python\Python37-32\python.exe
+
+'''
+For learning print function in Python 
+By Suresh
+Python ver 3.7
+On date 25-Nov-2018
+'''
+import thirdpython
+
+#This is to execute a function in thirdpython module
+thirdpython.mobile_brands_list_display_func()
