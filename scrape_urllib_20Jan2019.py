a812c0d05fc98900f1136293a2ad67cd25e0f42e
PolarisSuresh <polaris_suresh@yahoo.com>
2019-01-20 22:26:34 +0530

    Latest version 01202019

--- scrape_urllib_20Jan2019.py
+++ scrape_urllib_20Jan2019.py
@@ -5,9 +5,26 @@ By Suresh
 Python ver 3.7
 On date 31-Dec-2018, revised on 20-Jan-2018
 '''
-import urllib.request
+import time
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-#Collect and parse first page
-request_url = urllib.request.urlopen('https://www.besanttechnologies.com/')
-print(request_url.read())
-
+url= "http://toscrape.com/"
+html=urlopen(url)
+soup=BeautifulSoup(html.read(),"html.parser")
+Text1=soup.h1
+HeaderText=Text1.get_text()
+body=soup.find('body')
+body1=body.findChildren()
+list=soup.prettify()
+list=soup.find_all('td')
+n=0
+while (n < 3):
+   string=list[n]
+   newstring=string.strip()
+   print('|',newstring,'|',len(newstring),'|')
+   if 'of' in newstring:
+     print('Yes')
+   n=n+1
+    
