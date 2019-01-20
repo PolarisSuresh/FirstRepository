d34ffecb7df899434ff5675c320aeb1f3c299e76
PolarisSuresh <polaris_suresh@yahoo.com>
2019-01-20 13:59:13 +0530

    Fifth Python

---
+++ fifthpython.py
@@ -0,0 +1,23 @@
+#!C:\Users\Suresh\AppData\Local\Programs\Python\Python37-32\python.exe
+
+'''
+For learning print function in Python 
+By Suresh
+Python ver 3.7
+On date 16-Dec-2018
+'''
+
+#This is to check data types
+
+name="Biography of D' Souza & D'Silva"
+number1=123456789
+float1=33.999
+realimag=2+4j
+print("Name is:",name)
+print('Number:' ,number1)
+print('Float: ',float1)
+print('Some scientific value-',realimag)
+print('Data types')
+print(type(name))
+print(type(float1))
+
